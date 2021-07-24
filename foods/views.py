c05c3865790ba7cc3c47c5d03278d6cbd468f491
이민진 <e.minjin00@gmail.com>
2021-07-24 23:30:38 +0900

    Feat: Elegent url in foods views

--- foods/views.py
+++ foods/views.py
@@ -7,3 +7,7 @@ def index(request):
     today = datetime.today().date()
     context = {"date" : today}
     return render(request, 'foods/index.html', context=context)
+
+def food_detail(request, food):
+    context = {"name" : food}
+    return render(request, 'foods/detail.html', context=context)
