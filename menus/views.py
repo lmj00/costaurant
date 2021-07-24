d212b12b0db6dd97ca858b92aa9513dda43247b9
이민진 <e.minjin00@gmail.com>
2021-07-24 15:15:07 +0900

    Feat: dynamic time in menus views

--- menus/views.py
+++ menus/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+import time
+from time import localtime, strftime
+from django.utils import timezone
 
 # Create your views here.
-def index(request):
-    return render(request, 'menus/index.html')
+def index(request): 
+    today = datetime.today().date()
+    context = {"date" : today}
+    return render(request, 'menus/index.html', context = context)
