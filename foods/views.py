4e073fe928b88c4110905481dcb13f902e7633be
이민진 <e.minjin00@gmail.com>
2021-08-02 02:24:27 +0900

    Feat: get pk id in foods views

--- foods/views.py
+++ foods/views.py
@@ -13,14 +13,8 @@ def index(request):
     context["menus"] = menus
     return render(request, 'foods/index.html', context=context)
 
-def food_detail(request, food):
+def food_detail(request, pk):
     context = dict()
-    if food == "chicken":
-        context["name"] = "코딩에 빠진 닭"
-        context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격!"
-        context["price"] = 10000
-        context["img_path"] = "foods/images/chicken.jpg"
-    else:
-        raise Http404("이런 음식은 없습니다.")
-
+    menu = Menu.objects.get(id=pk)
+    context["menu"] = menu
     return render(request, 'foods/detail.html', context=context)
