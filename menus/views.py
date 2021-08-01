bd6ac9dfac454cd3642e3b41781371b3306b3573
이민진 <e.minjin00@gmail.com>
2021-08-02 04:27:20 +0900

    Feat: get pk id in menus views

--- menus/views.py
+++ menus/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-import time
-from time import localtime, strftime
-from django.utils import timezone
+from menus.models import Menu
 
 # Create your views here.
 def index(request): 
+    context = dict()
     today = datetime.today().date()
-    context = {"today" : today}
+    menus = Menu.objects.all()
+    context["today"] = today
+    context["menus"] = menus
     return render(request, 'menus/index.html', context = context)
 
-def menu_detail(request, menu):
-    return render(request,'menus/detail.html')
+def menu_detail(request, pk):
+    detail = Menu.objects.get(id=pk)
+    return render(request,'menus/detail.html', {'detail':detail})
+
+    
